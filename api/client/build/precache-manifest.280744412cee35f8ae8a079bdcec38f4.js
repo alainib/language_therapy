156d19cf3383c3ffaf3832ff80404a6634a36815
alainib <ibrahimalain@hotmail.com>
2019-11-24 23:28:13 +0100

    fix build

--- api/client/build/precache-manifest.280744412cee35f8ae8a079bdcec38f4.js
+++ api/client/build/precache-manifest.280744412cee35f8ae8a079bdcec38f4.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d396d06e2856022b10ca5327a116fc43",
+    "revision": "37eb4865fd884e940cf9d48172f5e944",
     "url": "/index.html"
   },
   {
-    "revision": "26103d2cb9c5ae0606d1",
+    "revision": "be70a42cdaa14b05a5b1",
     "url": "/static/css/2.e975ae8e.chunk.css"
   },
   {
-    "revision": "c4655062ad19054615b0",
+    "revision": "5453f3efc4e6dfd3705d",
     "url": "/static/css/main.916924c1.chunk.css"
   },
   {
-    "revision": "26103d2cb9c5ae0606d1",
-    "url": "/static/js/2.314500b5.chunk.js"
+    "revision": "be70a42cdaa14b05a5b1",
+    "url": "/static/js/2.b9b415b7.chunk.js"
   },
   {
-    "revision": "c4655062ad19054615b0",
-    "url": "/static/js/main.1f90070d.chunk.js"
+    "revision": "5453f3efc4e6dfd3705d",
+    "url": "/static/js/main.ce09356c.chunk.js"
   },
   {
     "revision": "64ab56e38a1c971639bb",
