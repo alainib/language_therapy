7146877c7b1dbfa1e15124c79459a9abe620dbbe
alain <ibrahimalain@hotmail.com>
2019-11-24 11:59:59 +0100

    new build fix api token

---
+++ express-api/build/precache-manifest.db2639acf14a390f127316d2730205ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb31f2f4379cb4c73c9ce3288bcd6d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "889a9eb52701f4391396",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "78692f2eb3285d079b3f",
+    "url": "/static/css/main.e78fada2.chunk.css"
+  },
+  {
+    "revision": "889a9eb52701f4391396",
+    "url": "/static/js/2.d9bdd827.chunk.js"
+  },
+  {
+    "revision": "78692f2eb3285d079b3f",
+    "url": "/static/js/main.456a3891.chunk.js"
+  },
+  {
+    "revision": "e9306b9d7b6c9fb9d9cd",
+    "url": "/static/js/runtime-main.8e8a4ec5.js"
+  }
+]);
